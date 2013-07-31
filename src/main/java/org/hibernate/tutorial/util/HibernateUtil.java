1d6cf7b8a58b659cf24233614282b0fda751c08d
Manuel PAYET <manuelpayet@gmail.com>
2013-07-31 15:32:30 +0200

    Suppression d'un warning sur une méthode dépréciée

--- src/main/java/org/hibernate/tutorial/util/HibernateUtil.java
+++ src/main/java/org/hibernate/tutorial/util/HibernateUtil.java
@@ -7,6 +7,7 @@ public class HibernateUtil {
 
   private static final SessionFactory sessionFactory = buildSessionFactory();
 
+  @SuppressWarnings("deprecation")
   private static SessionFactory buildSessionFactory() {
     try {
       // Create the SessionFactory from hibernate.cfg.xml
